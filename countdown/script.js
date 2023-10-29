1fea35c40df003b488f37b21bed42fc0446d95f9
ifmsabrazilunifor <148591427+ifmsabrazilunifor@users.noreply.github.com>
2023-10-29 13:45:31 -0300

    Create script.js

---
+++ countdown/script.js
@@ -0,0 +1,18 @@
+const targetDate = new Date('2023-12-14T21:15:00Z'); // GMT-3 is 3 hours behind GMT, so we add 3 hours to the target time
+
+function updateCountdown() {
+    const now = new Date();
+    const timeDiff = targetDate - now;
+
+    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+    document.getElementById('days').textContent = days.toString().padStart(2, '0');
+    document.getElementById('hours').textContent = hours.toString().padStart(2, '0');
+    document.getElementById('minutes').textContent = minutes.toString().padStart(2, '0');
+    document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
+}
+
+setInterval(updateCountdown, 1000);
